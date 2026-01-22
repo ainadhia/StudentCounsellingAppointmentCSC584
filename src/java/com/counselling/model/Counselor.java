0efedfe276a87e9e01ebec0665141e840cf0307d
anisazwani <anisazwani044@gmail.com>
2026-01-23 02:36:08 +0800

    add student profile dan dashboard pages

---
+++ src/java/com/counselling/model/Counselor.java
@@ -0,0 +1,31 @@
+package com.counselling.model;
+
+/**
+ *
+ * @author Aina
+ */
+public class Counselor extends User {
+    private String counselorID;
+    private String roomNo;
+    
+    // Constructors
+    public Counselor() {
+        super();
+    }
+    
+    public Counselor(String userID, String userName, String fullName, String userEmail,
+                    String userPassword, String userPhoneNum, 
+                    String counselorID, String roomNo) {
+        super(userID, userName, fullName, userEmail, userPassword, "counselor", userPhoneNum);
+        this.counselorID = counselorID;
+        this.roomNo = roomNo;
+    }
+    
+    // Getters and Setters
+    public String getCounselorID() { return counselorID; }
+    public void setCounselorID(String counselorID) { this.counselorID = counselorID; }
+    
+    public String getRoomNo() { return roomNo; }
+    public void setRoomNo(String roomNo) { this.roomNo = roomNo; }
+}
+
